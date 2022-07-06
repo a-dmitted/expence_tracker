243cd583d5e87aafa4acf33fc4868e8688afda22
a-dmitted <dmitriev.alexander@gmail.com>
2022-07-06 16:06:58 +0200

    CONFIG: setup GitHub actions

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,15 +5,20 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:expence_tracker/model/counter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:expence_tracker/main.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(ChangeNotifierProvider(
+      child: const MyApp(),
+      create: (context) => Counter(),
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
